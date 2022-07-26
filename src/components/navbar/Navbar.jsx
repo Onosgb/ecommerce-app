db830c7f3efa659ef489e4f0b7090eb889c65cd2
Onosgb <onosgb@gmail.com>
2022-07-26 20:00:44 +0100

    created navbar component and navbar styled component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import Badge from "@material-ui/core/Badge";
+
+import {
+  Wrapper,
+  Container,
+  Left,
+  Center,
+  Right,
+  Language,
+  SearchContainer,
+  Input,
+  Logo,
+  MenuItem,
+} from "./Navbar.styles";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
